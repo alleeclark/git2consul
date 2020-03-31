a3b743a42b189111f8a28f9c74a052c35ee15669
allee <alleeclark@gmail.com>
2020-03-30 22:17:32 -0400

    add support for config file

--- app/command/app.go
+++ app/command/app.go
@@ -2,36 +2,39 @@ package command
 
 import (
 	cli "github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v2/altsrc"
 )
 
 //New cli application for git2consul commands
 func New() *cli.App {
 	app := cli.NewApp()
 	app.Name = "git2consul"
-	app.Version = "0.0.3"
+	app.Version = "0.0.4"
 	app.Usage = "a syncing service from git to consul"
 
 	app.Flags = []cli.Flag{
-		&cli.StringFlag{Name: "git-user", Value: "git2consul", Usage: "git username", Required: false},
+		altsrc.NewStringFlag(&cli.StringFlag{Name: "git-user", Value: "git2consul", Usage: "git username", Required: false}),
 		&cli.StringFlag{Name: "git-password", Value: "", Usage: "git password", Required: false},
-		&cli.StringFlag{Name: "git-url", Usage: "git url to clone", Required: false},
-		&cli.StringFlag{Name: "git-branch", Value: "master", Usage: "git branch to run syncing on"},
-		&cli.StringFlag{Name: "git-dir", Value: "/var/git2consul/data", Usage: "directory to pull to"},
-		&cli.StringFlag{Name: "git-ssh-publickey-path", Usage: "public key for ssh agent"},
-		&cli.StringFlag{Name: "git-ssh-privatekey-path", Usage: "private key for ssh agent"},
-		&cli.StringFlag{Name: "git-ssh-passphrase-path", Usage: "passpharse for sshkey"},
-		&cli.StringFlag{Name: "git-remote", Value: "origin"},
-		&cli.BoolFlag{Name: "git-ssh-file", Usage: "read public, private, and passpharse for ssh agent", Hidden: true},
-		&cli.StringFlag{Name: "git-fingerprint-path", FilePath: "/var/git2consul/.ssh/fingerprint", Usage: "git RSA finerprint id", Required: false},
-		&cli.StringFlag{Name: "consul-addr", Value: "localhost:8500", EnvVars: []string{"CONSUL_ADDR"}, Usage: "consul address to write to. Will use agent unless an env is set of CONSUL_ADDR"},
-		&cli.StringFlag{Name: "consul-path", Value: "", Usage: "consul path to sync "},
+		altsrc.NewStringFlag(&cli.StringFlag{Name: "git-url", Usage: "git url to clone", Required: false}),
+		altsrc.NewStringFlag(&cli.StringFlag{Name: "git-branch", Value: "master", Usage: "git branch to run syncing on"}),
+		altsrc.NewStringFlag(&cli.StringFlag{Name: "git-dir", Value: "/var/git2consul/data", Usage: "directory to pull to"}),
+		altsrc.NewStringFlag(&cli.StringFlag{Name: "git-ssh-publickey-path", Usage: "public key for ssh agent"}),
+		altsrc.NewStringFlag(&cli.StringFlag{Name: "git-ssh-privatekey-path", Usage: "private key for ssh agent"}),
+		altsrc.NewStringFlag(&cli.StringFlag{Name: "git-ssh-passphrase-path", Usage: "passpharse for sshkey"}),
+		altsrc.NewStringFlag(&cli.StringFlag{Name: "git-remote", Value: "origin"}),
+		altsrc.NewBoolFlag(&cli.BoolFlag{Name: "git-ssh-file", Usage: "read public, private, and passpharse for ssh agent", Hidden: true}),
+		altsrc.NewStringFlag(&cli.StringFlag{Name: "git-fingerprint-path", FilePath: "/var/git2consul/.ssh/fingerprint", Usage: "git RSA finerprint id", Required: false}),
+		altsrc.NewStringFlag(&cli.StringFlag{Name: "consul-addr", Value: "localhost:8500", EnvVars: []string{"CONSUL_ADDR"}, Usage: "consul address to write to. Will use agent unless an env is set of CONSUL_ADDR"}),
+		altsrc.NewStringFlag(&cli.StringFlag{Name: "consul-path", Value: "", Usage: "consul path to sync "}),
 		&cli.StringFlag{Name: "consul-token", Value: "somestillytoken", EnvVars: []string{"CONSUL_TOKEN"}, Usage: "consul address to write to. Will use agent unless an env is set of CONSUL_TOKEN"},
-		&cli.BoolFlag{Name: "metrics", Usage: "send metrics to pushgateway", EnvVars: []string{"GIT2CONSUL_METRICS"}, Hidden: true},
-		&cli.StringFlag{Name: "pushgateway-addr", Value: "localhost:9091", Usage: "push gateway address for metrics", Hidden: true},
-		&cli.StringFlag{Name: "log-level,l", Usage: "set the logging level [trace, debug, info, warn, error, fatal, panic]", Value: "debug"},
-		&cli.StringFlag{Name: "log-file", Usage: "logfile path", Value: "/var/git2consul/logs/git2consul.log"},
-		&cli.StringFlag{Name: "log-format", Usage: "json", Value: "text"},
+		altsrc.NewBoolFlag(&cli.BoolFlag{Name: "metrics", Usage: "send metrics to pushgateway", EnvVars: []string{"GIT2CONSUL_METRICS"}, Hidden: true}),
+		altsrc.NewStringFlag(&cli.StringFlag{Name: "pushgateway-addr", Value: "localhost:9091", Usage: "push gateway address for metrics", Hidden: true}),
+		altsrc.NewStringFlag(&cli.StringFlag{Name: "log-level,l", Usage: "set the logging level [trace, debug, info, warn, error, fatal, panic]", Value: "debug"}),
+		altsrc.NewStringFlag(&cli.StringFlag{Name: "log-file", Usage: "logfile path", Value: "/var/git2consul/logs/git2consul.log"}),
+		altsrc.NewStringFlag(&cli.StringFlag{Name: "log-format", Usage: "json", Value: "text"}),
+		&cli.StringFlag{Name: "config-file,c", Usage: "configuration file to read non sensative variables from must be in toml format", Value: "/var/git2consul/config.toml"},
 	}
+	app.Before = altsrc.InitInputSourceWithContext(app.Flags, altsrc.NewTomlSourceFromFlagFunc("config-file"))
 	app.Commands = []*cli.Command{&operatorCommand, &syncCommand, &resyncCommand}
 	return app
 }
